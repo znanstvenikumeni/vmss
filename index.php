19da3eb0627b7b4e42e20788ba8502e1ebb615e8
Mario Borna Mjertan <mbm@educateam.hr>
2019-08-21 20:55:42 +0200

    Setting a default CORS policy.

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
-
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+    header('Access-Control-Allow-Origin: *');
+    die();
+}
+header('Access-Control-Allow-Origin: *');
 include 'vmss/vmss.php';
 $vmss = new vmssCore;
 include 'vmss/router.php';
